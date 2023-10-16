bb7355732aed9c5d8956c6bbbdbd80fc4052f4bb
dequipeth <39134355+dequipeth@users.noreply.github.com>
2023-10-16 17:16:52 +0200

    Creation of a function to turn hex to rgb.

--- prompt.sh
+++ prompt.sh
@@ -6,6 +6,24 @@
 
 script_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
+
+hex_to_rgb() {
+  
+  hex="$(echo "$1" | sed 's/#//g')"
+
+  r="$(echo "$hex" | cut -c-2)"
+  g="$(echo "$hex" | cut -c3-4)"
+  b="$(echo "$hex" | cut -c5-6)"
+
+  r="$(echo "ibase=16; $r" | bc)"
+  g="$(echo "ibase=16; $g" | bc)"
+  b="$(echo "ibase=16; $b" | bc)"
+
+  echo $1
+  echo $r $g $b
+}
+
+
 fg_black="\[\033[38;2;30m\]"
 fg_red="\[\033[38;2;31m\]"
 fg_green="\[\033[38;2;32m\]"
