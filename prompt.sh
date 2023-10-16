5a30c30f5b3a3f404ad44e70868cc99b4be22a60
dequipeth <39134355+dequipeth@users.noreply.github.com>
2023-10-16 12:42:18 +0200

    Modification of the prompt theme.

--- prompt.sh
+++ prompt.sh
@@ -1,18 +1,22 @@
-#!/bin/bash
+!/bin/bash
 # Configures terminal prompt theme.
 #
 # Thomas DEQUIPE
 
 
-black="30"
-red="31"
-green="32"
-yellow="33"
-blue="34"
-magenta="35"
-cyan="36"
-white="37"
+script_dir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
+fg_black="\[\033[38;2;30m\]"
+fg_red="\[\033[38;2;31m\]"
+fg_green="\[\033[38;2;32m\]"
+fg_yellow="\[\033[38;2;33m\]"
+fg_blue="\[\033[38;2;34m\]"
+fg_magenta="\[\033[38;2;35m\]"
+fg_cyan="\[\033[38;2;36m\]"
+fg_white="\[\033[38;2;37m\]"
+fg_end="\[\033[00m\]"
 
-PS1='\[\033[01;32m\]┌──${debian_chroot:+($debian_chroot)}(\[\033[00m\]\u@\h\[\033[01;32m\])-[\[\033[01;34m\]\w\[\033[01;32m\]]\n└─\[\033[00m\]\$'
+line_1="${fg_blue}┌───( \u@\h )-[ \w ]"
+line_2="└─\$${fg_end} "
 
+PS1="\r\n${line_1}\r\n${line_2}"
