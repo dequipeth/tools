f4180f62f5778e77c3e7241e43551622e1a7fa4a
tdequipe <thomas.dequipe@gmail.com>
2023-11-28 00:18:03 +0100

    Modification of the Debian post-install script.

--- debian/post-install.sh
+++ debian/post-install.sh
@@ -2,25 +2,34 @@
 # This script performs the post-installation tasks of Debian.
 # It must be executed as root.
 
-# Update packages
+
+## Update packages
 apt-get update && apt-get upgrade -y
 
-# Install KDE packages
-apt-get install plasma-desktop \
-  kwin-x11 plasma-workspace-wayland \
-  plymouth-theme-breeze \
-  sddm kde-config-sddm sddm-theme-debian-breeze \
-  kde-config-screenlocker \
-  plasma-nm systemsettings \
-  dolphin kate konsole --no-install-recommends -y
 
-# Install X Server
-apt-get install xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all xinit x11-xserver-utils --no-install-recommends --assume-yes
+## Install system utilities
+apt-get install git vim -y
 
-# Install KDE
-apt-get install plasma-desktop --no-install-recommends --assume-yes
-apt-get install plasma-workspace-wayland --no-install-recommends --assume-yes
-apt-get install sddm --no-install-recommends --assume-yes
-apt-get install plasma-nm --no-install-recommends --assume-yes
-apt-get install konsole --no-install-recommends --assume-yes
-apt-get install dolphin --no-install-recommends --assume-yes
+
+## Install X Server
+apt-get install xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all xinit x11-xserver-utils --no-install-recommends -y
+
+
+## Install KDE
+
+# KDE Plasma Desktop
+apt-get install plasma-desktop --no-install-recommends -y
+# KDE Plasma Wayland
+apt-get install plasma-workspace-wayland --no-install-recommends -y
+# Simple Desktop Display Manager 
+apt-get install sddm kde-config-sddm sddm-theme-debian-breeze --no-install-recommends -y
+# KDE Screen Manager
+apt-get install kscreen --no-install-recommends -y
+# KDE Plasma Network Manager
+apt-get install plasma-nm --no-install-recommends -y
+# KDE Screen Locker
+apt-get install kde-config-screenlocker --no-install-recommends -y
+# KDE Console Emulator
+apt-get install konsole --no-install-recommends -y
+# KDE File Manager
+apt-get install dolphin --no-install-recommends -y
