470a551777ca3f3efb05738643c5193f116904b3
dequipeth <39134355+dequipeth@users.noreply.github.com>
2023-11-27 18:10:41 +0100

    Update post-install.sh

--- debian/post-install.sh
+++ debian/post-install.sh
@@ -13,3 +13,14 @@ apt-get install plasma-desktop \
   kde-config-screenlocker \
   plasma-nm systemsettings \
   dolphin kate konsole --no-install-recommends -y
+
+# Install X Server
+apt-get install xserver-xorg-core xserver-xorg-input-all xserver-xorg-video-all xinit x11-xserver-utils --no-install-recommends --assume-yes
+
+# Install KDE
+apt-get install plasma-desktop --no-install-recommends --assume-yes
+apt-get install plasma-workspace-wayland --no-install-recommends --assume-yes
+apt-get install sddm --no-install-recommends --assume-yes
+apt-get install plasma-nm --no-install-recommends --assume-yes
+apt-get install konsole --no-install-recommends --assume-yes
+apt-get install dolphin --no-install-recommends --assume-yes
